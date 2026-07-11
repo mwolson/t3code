0e0322ac06810267f3beb897510ee45e3b5e6fb7
Jake Leventhal <jakeleventhal@me.com>
2026-07-11 18:59:00 -0400

    Hide completion sounds behind toggle

--- apps/desktop/src/settings/DesktopClientSettings.test.ts
+++ apps/desktop/src/settings/DesktopClientSettings.test.ts
@@ -23,6 +23,7 @@ const clientSettings: ClientSettings = {
   dismissedProviderUpdateNotificationKeys: [],
   diffIgnoreWhitespace: true,
   environmentIdentificationMode: "artwork",
+  enableCompletionSounds: false,
   favorites: [],
   fontFamilyCode: "",
   fontFamilyComposer: "",
